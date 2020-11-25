61645a8d753957e8cecd49145e05b2be1418bb22
Romain Boisselle <romain@boisselle.dev>
2020-11-25 07:10:08 +0100

    kotlin 1.4.20 + kx serialization 1.0.0 + Log 0.7.0 + Memory + 0.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 
 allprojects {
     group = "org.kodein.db"
-    version = "0.4.0-beta"
+    version = "0.4.1-beta"
 
     repositories {
         mavenLocal()
@@ -27,9 +27,9 @@ allprojects {
 }
 
 val kotlinxAtomicFuVer by extra { "0.14.4" } // CAUTION: also change in buildscript!
-val kotlinxSerializationVer by extra { "1.0.0-RC" }
-val kodeinLogVer by extra { "0.6.0" }
-val kodeinMemoryVer by extra { "0.4.0" }
+val kotlinxSerializationVer by extra { "1.0.0" }
+val kodeinLogVer by extra { "0.7.0" }
+val kodeinMemoryVer by extra { "0.4.1" }
 
 val androidNdkVer by extra { "21.0.6113669" } // CAUTION: also change in CI workflows!
 
