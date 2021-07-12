0a7128ceb227400fe3a3606cf9912f35985a2dd3
Jakob K <bluefireolymp@gmail.com>
2021-07-12 20:12:43 +0200

    Add easy to use index filters

---
+++ kdb/kodein-db-api/src/commonMain/kotlin/org/kodein/db/model/index/IndexFilters.kt
@@ -0,0 +1,53 @@
+package org.kodein.db.model.index
+
+import org.kodein.db.Cursor
+import org.kodein.db.DBRead
+import org.kodein.db.find
+import kotlin.reflect.KProperty1
+
+public abstract class Filter<M : Any> {
+    public abstract fun find(db: DBRead): Cursor<M>
+}
+
+/**
+ * Creates a new filter matching all elements where the
+ * content of the index equals the given [value].
+ */
+public inline infix fun <reified M : Any, T : Any> KProperty1<M, IndexSingleDefinition<T>>.eq(value: T): Filter<M> =
+    object : Filter<M>() {
+        override fun find(db: DBRead) = db.find<M>().byIndex(this@eq.name, value)
+    }
+
+/**
+ * Creates a new filter matching all elements where the content
+ * of the index equals the given [pair] of values.
+ */
+public inline infix fun <reified M : Any, A : Any, B : Any> KProperty1<M, IndexPairDefinition<A, B>>.eq(pair: Pair<A, B>): Filter<M> =
+    object : Filter<M>() {
+        override fun find(db: DBRead) = db.find<M>().byIndex(this@eq.name, pair.first, pair.second)
+    }
+
+/**
+ * Creates a new filter matching all elements where the content
+ * of the index equals the given [triple] of values.
+ */
+public inline infix fun <reified M : Any, A : Any, B : Any, C : Any> KProperty1<M, IndexTripleDefinition<A, B, C>>.eq(triple: Triple<A, B, C>): Filter<M> =
+    object : Filter<M>() {
+        override fun find(db: DBRead) = db.find<M>().byIndex(this@eq.name, triple.first, triple.second, triple.third)
+    }
+
+/**
+ * Creates a new filter matching all elements where the content
+ * of the composite index matches the given composite index values.
+ *
+ * Note: the array does not have to satisfy all values of the composite index,
+ * it may be incomplete
+ *
+ * Attention: creating this filter is not typesafe, please make sure that all
+ * the given types are valid (the same as the types of the properties of the
+ * composite index)
+ */
+public inline infix fun <reified M : Any> KProperty1<M, IndexCompositeDefinition>.eq(values: Array<Any>): Filter<M> =
+    object : Filter<M>() {
+        override fun find(db: DBRead) = db.find<M>().byIndex(this@eq.name, *values)
+    }
