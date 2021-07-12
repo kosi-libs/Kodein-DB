be47d1988b4871b242db16ab4e0432e0dc10e00f
Jakob K <bluefireolymp@gmail.com>
2021-07-12 20:33:33 +0200

    Add update extension functions for new index filters

---
+++ kdb/kodein-db-api/src/commonMain/kotlin/org/kodein/db/model/index/IndexUpdates.kt
@@ -0,0 +1,10 @@
+package org.kodein.db.model.index
+
+import org.kodein.db.DB
+
+/**
+ * Deletes all elements in this database matching the given [filter].
+ */
+public inline fun <reified M : Any> DB.delete(filter: Filter<M>) {
+    filter.delete(this)
+}
